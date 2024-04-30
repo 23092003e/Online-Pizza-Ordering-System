dabc3697b3739b93eb29cd3d62dfba1bbdf4caa0
HoangVanManh <hoangvanmanh2309@gmail.com>
2024-04-30 08:05:07 +0700

    Update

--- src/customer.java
+++ src/customer.java
@@ -2,14 +2,14 @@ public class customer
 {
     String name;             // stores name of the customer
     String address;          // stores address of the customer (house no, building name)
-    String mobile_number;        // stores mobile number of the customer
+    String mobile_no;        // stores mobile number of the customer
     int location;            // stores an integer denoting the delivery location of the customer
 
-    public customer(String name, String address, int location, String mn)
+    public customer(String n, String adr, int loc, String mn)
     {
-        this.name=name;
-        this.address= address;
-        this.mobile_number= mn;
-        this.location = location;
+        this.name=n;
+        this.address= adr;
+        this.mobile_no= mn;
+        this.location = loc;
     }
 }
